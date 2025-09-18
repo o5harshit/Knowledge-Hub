a537da520fa243342bd49acb2aeb24b0ea6d10aa
o5harshit <srivastavaharshit400@gmail.com>
2025-09-18 17:37:12 +0530

    changed index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv"
 import AuthRoutes from "./Routes/AuthRoutes.js";
 import DocumentRoutes from "./Routes/DocumentRoutes.js";
-import gemini from "./Config/gemini.js";
 import GeminiRoutes from "./Routes/GeminiRoutes.js";
 
 
